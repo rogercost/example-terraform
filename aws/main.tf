ed7d6bd6978ece64ae0a7dc9597c95819d71e40e
Ali <ali@infracost.io>
2024-06-14 11:05:20 -0700

    feat: add more resources

--- aws/main.tf
+++ aws/main.tf
@@ -34,3 +34,24 @@ resource "aws_lambda_function" "my_hello_world" {
   }
 }
 
+resource "aws_instance" "new_web_app" {
+  ami           = "ami-005e54dee72cc1d00"
+
+  instance_type = "m5.4xlarge"
+
+  tags = {
+    Environment = "production"
+    Service     = "web-app"
+  }
+
+  root_block_device {
+    volume_size = 100
+  }
+
+  ebs_block_device {
+    volume_size = 3000
+    volume_type = "io2"
+    iops = "20000"
+  }
+}
+
